d724643f7b7ec6ebe153ddd3f96087bdf0b34351
Nimoscmp <nimos.cmp@gmail.com>
2021-08-13 18:25:39 -0500

    Structure main banner and styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import MainBanner from "./components/MainBanner";
+import './styles/sass/styles.css';
 
 function App() {
   return (
     <>
+      <main className="main">
+        <MainBanner/>
+      </main>
     </>
   );
 }
---
+++ src/components/MainBanner.jsx
@@ -0,0 +1,14 @@
+import '../styles/sass/styles.css';
+
+const MainBanner = () => {
+    return (
+    <>
+        <section className="main-banner">
+            <i class="fas fa-map-marker"></i>
+            <h5>Bogotá</h5>
+        </section>           
+    </>
+    )
+}
+
+export default MainBanner;
