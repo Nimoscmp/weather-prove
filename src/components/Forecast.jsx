024cb68a68eaf3c30550f867b6618837e5e34f06
Nimoscmp <nimos.cmp@gmail.com>
2021-08-15 16:31:59 -0500

    Set api for forecast

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,8 +1,15 @@
-const Forecast = () => {
+import { useEffect, useState } from 'react';
+import { calculateDay } from '../utils/calculations';
 
+const Forecast = () => {
+    //  Constants
     const today = new Date();
     let stringDay;
 
+    //  Local states
+    const [response, setResponse] = useState();
+
+    //  Day names
     const selectDay = (day) => {
 
         let theDay;
@@ -13,35 +20,36 @@ const Forecast = () => {
             theDay = day;
         }
 
-        switch (theDay) {
-            case 1:
-                stringDay = 'Monday';
-                break;
-            case 2:
-                stringDay = 'Tuesday';
-                break;
-            case 3:
-                stringDay = 'Wednesday';
-                break;
-            case 4:
-                stringDay = 'Thursday';
-                break;
-            case 5:
-                stringDay = 'Friday';
-                break;
-            case 6:
-                stringDay = 'Saturday';
-                break;
-            case 7:
-                stringDay = 'Sunday';
-                break;
-            default:
-                stringDay = '';
-                break;
-        }
+        stringDay = calculateDay(theDay, 'eng');
 
         return stringDay;
     }
+
+    useEffect(() => {
+        const getData = () => {
+            // const lat = '4.707'
+            // const lon = '-74.107'
+            // const api_key = '0b79dea464b8f769b18696de8de31770';
+            // const base_url = `https://api.openweathermap.org/data/2.5/onecall?lat=${ lat }&lon=${ lon }&exclude=minutely,hourly&appid=${ api_key }&units=metric`;
+            // // const base_url = `https://api.openweathermap.org/data/2.5/weather?q=Paris&lang=es&appid=${ api_key }&units=metric`;
+            // const xhttp = new XMLHttpRequest();
+            // var json;
+        
+            // xhttp.open('GET', base_url, true);
+        
+            // xhttp.send();
+        
+            // xhttp.onreadystatechange = function() {
+            //     if (this.readyState === 4 && this.status === 200) {
+            //         json = JSON.parse(this.response);
+            //         setResponse(json);
+            //     }
+            // }
+        }
+        // getData();
+        console.log(response);
+        // eslint-disable-next-line
+    }, [])
 
     return (
     <>
@@ -51,52 +59,52 @@ const Forecast = () => {
             <div className="days">
                 <div className="sub-section">
                     <img 
-                        src={'https://openweathermap.org/img/w/10d.png'} 
+                        src={`https://openweathermap.org/img/w/${response?.daily[0].weather[0].icon}.png`} 
                         alt="weather1"
                         className="" />
                 </div>
                 <div className="sub-section">
                     <h5 className="day">{selectDay(today.getDay() + 1)}</h5>
-                    <p className="description"><strong>Cloudy</strong></p>
+                    <p className="description"><strong>{response?.daily[0].weather[0].description}</strong></p>
                 </div>
                 <div className="sub-section">
-                    <span className="temp-max">15&#8451;</span>
+                    <span className="temp-max">{Math.round(response?.daily[0].temp.max)}&#8451;</span>
                     <span className="temp">/</span>
-                    <span className="temp-min">10&#8451;</span>
+                    <span className="temp-min">{Math.round(response?.daily[0].temp.min)}&#8451;</span>
                 </div>
             </div>
             <div className="days">
                 <div className="sub-section">
                     <img 
-                        src={'https://openweathermap.org/img/w/10d.png'} 
+                        src={`https://openweathermap.org/img/w/${response?.daily[1].weather[0].icon}.png`}
                         alt="weather2"
                         className="" />
                 </div>
                 <div className="sub-section">
                     <h5 className="day">{selectDay(today.getDay() + 2)}</h5>
-                    <p className="description"><strong>Rainy</strong></p>
+                    <p className="description"><strong>{response?.daily[1].weather[0].description}</strong></p>
                 </div>
                 <div className="sub-section">
-                    <span className="temp-max">16&#8451;</span>
+                    <span className="temp-max">{Math.round(response?.daily[1].temp.max)}&#8451;</span>
                     <span className="temp">/</span>
-                    <span className="temp-min">9&#8451;</span>
+                    <span className="temp-min">{Math.round(response?.daily[1].temp.min)}&#8451;</span>
                 </div>
             </div>
             <div className="days">
                 <div className="sub-section">
                     <img 
-                        src={'https://openweathermap.org/img/w/10d.png'} 
+                        src={`https://openweathermap.org/img/w/${response?.daily[2].weather[0].icon}.png`} 
                         alt="weather3"
                         className="" />
                 </div>
                 <div className="sub-section">
-                    <h5 className="day">Wednesday</h5>
-                    <p className="description"><strong>Sunny</strong></p>
+                    <h5 className="day">{selectDay(today.getDay() + 3)}</h5>
+                    <p className="description"><strong>{response?.daily[2].weather[0].description}</strong></p>
                 </div>
                 <div className="sub-section">
-                    <span className="temp-max">19&#8451;</span>
+                    <span className="temp-max">{Math.round(response?.daily[2].temp.max)}&#8451;</span>
                     <span className="temp">/</span>
-                    <span className="temp-min">12&#8451;</span>
+                    <span className="temp-min">{Math.round(response?.daily[2].temp.min)}&#8451;</span>
                 </div>
             </div>
         </section>        
