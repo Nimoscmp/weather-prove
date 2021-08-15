3ecccc7c01050d4bf88bb12c84a7f2e8081aaa37
Nimoscmp <nimos.cmp@gmail.com>
2021-08-15 15:53:54 -0500

    Set api with ajax for paris banner

--- src/components/ParisBanner.jsx
+++ src/components/ParisBanner.jsx
@@ -1,4 +1,34 @@
+import { useEffect, useState } from "react"
+
 const ParisBanner = () => {
+
+    const [response, setResponse] = useState();
+
+    useEffect(() => {
+        const getData = () => {
+
+            const api_key = '0b79dea464b8f769b18696de8de31770';
+            // const base_url = `https://api.openweathermap.org/data/2.5/onecall?lat=${ lat }&lon=${ lon }&exclude=minutely,hourly&appid=${ api_key }&units=${ units }`;
+            const base_url = `https://api.openweathermap.org/data/2.5/weather?q=Paris&lang=es&appid=${ api_key }&units=metric`;
+            const xhttp = new XMLHttpRequest();
+            var json;
+        
+            xhttp.open('GET', base_url, true);
+        
+            xhttp.send();
+        
+            xhttp.onreadystatechange = function() {
+                if (this.readyState === 4 && this.status === 200) {
+                    json = JSON.parse(this.response);
+                    setResponse(json);
+                }
+            }
+        }
+        getData();
+        console.log(response);
+        // eslint-disable-next-line
+    }, [])
+
     return (
     <>
         <section className="paris-banner">
@@ -7,22 +37,22 @@ const ParisBanner = () => {
                 <i className="fas fa-map-marker-alt fa-2x m-05"></i>
                 <span className="city">Paris</span>
                 <img 
-                    src={'https://openweathermap.org/img/w/10d.png'} 
+                    src={`https://openweathermap.org/img/w/${ response?.weather[0].icon }.png`} 
                     alt="weather"
                     className="" />
             </div>
             <div className="data">
                 <div className="data-item">
                     <h5 className="h5">Temperatura</h5>
-                    <span className="temp">29 &#8451;</span>
+                    <span className="temp">{Math.round(response?.main.temp)}&#8451;</span>
                 </div>
                 <div className="data-item">
                     <h5 className="h5">Sensación térmica</h5>
-                    <span className="temp">30 &#8451;</span>
+                    <span className="temp">{Math.round(response?.main.feels_like)}&#8451;</span>
                 </div>
                 <div className="data-item">
                     <h5 className="h5">Nubosidad</h5>
-                    <span className="temp">81%</span>
+                    <span className="temp">{Math.round(response?.clouds.all)}%</span>
                 </div>
             </div>
         </section>            
